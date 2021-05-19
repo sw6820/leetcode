533c614ebf9e208cc22eb5a81938015b291c2002
sw6820 <52646313+sw6820@users.noreply.github.com>
2021-05-19 17:02:53 +0900

    Time: 52 ms (50.43%), Space: 14.3 MB (23.05%) - LeetHub

---
+++ flipping-an-image/flipping-an-image.py
@@ -0,0 +1,8 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i]=image[i][::-1]
+            for j in range(len(image)):
+                image[i][j]^=1
+        return image
+            
