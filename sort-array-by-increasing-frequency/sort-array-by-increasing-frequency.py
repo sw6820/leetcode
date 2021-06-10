906fda187168533f80898a74b5c1333434f41608
sw6820 <52646313+sw6820@users.noreply.github.com>
2021-06-11 00:35:08 +0900

    Time: 48 ms (81.05%), Space: 14.4 MB (17.70%) - LeetHub

---
+++ sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -0,0 +1,5 @@
+from collections import Counter
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        c=Counter(nums)
+        return sorted(nums, key=lambda x:[c[x],-x])
