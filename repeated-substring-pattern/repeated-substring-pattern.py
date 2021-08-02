561e5295b4867914a87720fb94f5a4f2b3083f7c
신원지_T2120 <52646313+sw6820@users.noreply.github.com>
2021-08-02 17:21:57 +0900

    Time: 116 ms (43.42%), Space: 14.4 MB (57.88%) - LeetHub

---
+++ repeated-substring-pattern/repeated-substring-pattern.py
@@ -0,0 +1,5 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        for i in range(1,len(s)//2+1):
+            if s==s[:i]*(len(s)//i): return True
+        return False
