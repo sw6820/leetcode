40b4b473c452b02accc013aa2ffd95abff70f06e
sw6820 <52646313+sw6820@users.noreply.github.com>
2021-10-04 20:14:46 +0900

    Time: 72 ms (79.98%), Space: 14.3 MB (55.52%) - LeetHub

--- max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
+++ max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.py
@@ -3,6 +3,5 @@ class Solution:
         l,cnt = len(grid),0
         r,c=[max(r) for r in grid],[max(c) for c in zip(*grid)]
         for i in range(l):
-            for j in range(l):
-                cnt+=min(r[i],c[j])-grid[i][j]
+            for j in range(l):cnt+=min(r[i],c[j])-grid[i][j]                
         return cnt
