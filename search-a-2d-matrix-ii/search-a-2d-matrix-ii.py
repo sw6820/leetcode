5bc6fcba74ccadaecd23444cf4456d9572291195
sw6820 <52646313+sw6820@users.noreply.github.com>
2021-10-16 19:42:21 +0900

    Time: 214 ms (22.80%), Space: 19.6 MB (50.11%) - LeetHub

---
+++ search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        return any(target in r for r in matrix)
