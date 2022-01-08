16cd5fa2baa944bb7354a0c7e8005a0de62f0563
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-01-09 00:26:21 +0900

    Time: 68 ms (8.43%), Space: 13.3 MB (92.70%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        return [i for i,v in enumerate(sorted(nums)) if target==v]
