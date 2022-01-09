b9b23fdac2a0860222f85f1188aca80cea440688
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-01-10 03:40:37 +0900

    Time: 76 ms (51.27%), Space: 13.7 MB (41.98%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def firstPalindrome(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        for i in words:
+            if i==i[::-1]: return i
+        return ""
