ebd4112b473f5ff61d358b9bba09e62af244f557
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-04-29 00:20:38 +0900

    Time: 19 ms (71.18%), Space: 13.3 MB (88.67%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -4,5 +4,5 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        n = sorted(list(str(num)))
+        n = sorted(str(num))
         return int(n[0]+n[-1])+int(n[1]+n[2])        
