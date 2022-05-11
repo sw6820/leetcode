21ce41d9ab5dea33c0e842d0b5e8817892b62117
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-05-11 16:42:57 +0900

    Time: 49 ms (30.85%), Space: 13.8 MB (63.74%) - LeetHub

--- 824-goat-latin/824-goat-latin.py
+++ 824-goat-latin/824-goat-latin.py
@@ -1,3 +1,3 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:        
-        return ' '.join((w if w[0].lower() in 'aeiou' else w[1:]+w[0])+'ma'+'a'*(i+1) for i,w in enumerate(sentence.split()))
+        return ' '.join((w if w[0].lower() in 'aeiou' else w[1:]+w[0])+'m'+'a'*(i+2) for i,w in enumerate(sentence.split()))
