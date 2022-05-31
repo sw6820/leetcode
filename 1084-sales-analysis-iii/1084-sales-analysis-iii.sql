391ad0b4c83d33a3217b93fc22ea56d9547a5c7e
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-05-31 22:35:40 +0900

    Time: 1051 ms (73.72%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,11 +1,8 @@
 # Write your MySQL query statement below
-
-# SELECT product_id, YEAR(sale_date), MONTH(sale_date), COUNT(*)
-#     FROM Sales
-#     GROUP BY product_id, YEAR(sale_date), MONTH(sale_date)
 SELECT P.product_id, product_name
 FROM Sales AS S
 INNER JOIN Product AS P
 ON P.product_id = S.product_id
 GROUP BY product_id
 HAVING 1<=MIN(MONTH(sale_date)) AND MAX(MONTH(sale_date))<=3
+###
