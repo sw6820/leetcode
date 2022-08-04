e956b658af3f9a98dca55e9f8c5983f8524aa526
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-08-05 02:06:32 +0900

    Time: 1421 ms (50.06%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT user_id AS buyer_id, join_date, COALESCE(COUNT(order_date),0) AS orders_in_2019
+FROM Users AS u
+LEFT JOIN Orders AS o
+ON user_id = buyer_id AND YEAR(order_date)=2019
+GROUP BY user_id
+###
