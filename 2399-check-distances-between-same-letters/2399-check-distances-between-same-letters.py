4fa15d1eef197dea4e9bb9b63390c21ff5dd9aee
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-09-14 17:10:43 +0900

    Time: 97 ms (13.03%), Space: 13.8 MB (72.46%) - LeetHub

--- 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.py
@@ -1,9 +1,8 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
         S=s[::-1]
+        st=set(s)
         for i in range(26):
-            if chr(ord('a')+i) not in s:continue
-            if (len(s)-S.index(chr(ord('a')+i)))-s.index(chr(ord('a')+i))-2!=distance[i]:return 0
-            
-        return 1
-            
+            if chr(ord('a')+i) not in st:continue
+            if (len(s)-S.index(chr(ord('a')+i)))-s.index(chr(ord('a')+i))-2!=distance[i]:return 0            
+        return 1            
