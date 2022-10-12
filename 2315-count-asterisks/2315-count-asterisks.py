cfec62b054353d6332b40eacf6187cc5a8a26495
sw6820 <52646313+sw6820@users.noreply.github.com>
2022-10-12 21:12:21 +0900

    Time: 63 ms (34.89%), Space: 13.9 MB (56.91%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.py
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -1,3 +1,4 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        return sum(w.count('*') for i,w in enumerate(s.split('|')) if not i&1)
+        # return sum(w.count('*') for i,w in enumerate(s.split('|')) if not i&1)
+        return sum(w.count('*') for w in s.split('|')[::2]) 
