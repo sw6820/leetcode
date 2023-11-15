257898a63568f541534372c254f8606a884107fe
sw6820 <52646313+sw6820@users.noreply.github.com>
2023-11-15 22:50:18 +0900

    Time: 434 ms (28.19%), Space: 18.6 MB (56.59%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,17 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        G,D=defaultdict(list),defaultdict(int)        
+        for u,v,w in times:
+            G[u].append((v,w))
+        c,m,q=0,0,[(0,k)]
+        while q:
+            t,u=heapq.heappop(q)
+            if u not in D:
+                D[u]=t
+                m=max(m,t)
+                c+=1
+                for v,w in G[u]:
+                    heapq.heappush(q,(t+w,v))        
+        return m if c==n else -1
+        
+        
