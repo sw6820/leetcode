3c5c64078d40572eca086a157b8b50f483ef68d3
sw6820 <52646313+sw6820@users.noreply.github.com>
2024-07-22 14:47:40 +0900

    Time: 56 ms (88.38%), Space: 49.5 MB (62.54%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -6,9 +6,9 @@
 var addTwoPromises = async function(promise1, promise2) {
     return await promise1 + await promise2;
     // return sum(Promise.all([promise1, promise2]));
-    // const [val1, val2] = Promise.all([promise1, promise2]);
-    // return val1+val2;
-    return Promise.all([promise1, promise2]).then((val1, val2)=>val1+val2);
+    const [val1, val2] = Promise.all([promise1, promise2]);
+    return val1+val2;
+    // return Promise.all([promise1, promise2]).then((val1, val2)=>val1+val2);
 };
 
 /**
