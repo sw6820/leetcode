a58e88f643fe47890f9d2785351800d37c628c10
sw6820 <52646313+sw6820@users.noreply.github.com>
2024-08-23 22:37:14 +0900

    Time: 47 ms (7.12%), Space: 16.5 MB (58.97%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,4 +1,5 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        return sum(abs(ord(s[i])-ord(s[i+1])) for i in range(len(s)-1))
+        # return sum(abs(ord(s[i])-ord(s[i+1])) for i in range(len(s)-1))
+        return sum(map(lambda x: abs(ord(x[0]) - ord(x[1])), zip(s, s[1:])))
         
