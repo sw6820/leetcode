14986a869d706b82aac1be9d6242910eba12dc87
sw6820 <52646313+sw6820@users.noreply.github.com>
2026-01-20 18:14:03 +0900

    Time: 117 ms (30.34%), Space: 20.5 MB (6.9%) - LeetHub

---
+++ 1410-html-entity-parser/1410-html-entity-parser.py
@@ -0,0 +1,27 @@
+class Solution:    
+    def entityParser(self, text: str) -> str:
+        d = {"&quot;": '"', "&apos;": "'", "&amp;":"&","&gt;":">","&lt;":"<","&frasl;":"/"}
+        s,L=0,len(text)
+        result = ''
+        while s<L:
+            t=text[s]
+            if t=='&':
+                f=1
+                for e in range(s+1,min(s+7,L)):
+                    if text[e]==';':
+                        tmp=text[s:e+1]
+                        if tmp in d:
+                            result+= d[tmp]
+                            s=e
+                        else:result+=t                            
+                        f=0
+                        break
+                if f:result+=t
+            else:result+=t       
+            s+=1
+        return result
+
+# "and I quote: &quot;...&quot;"
+# "leetcode.com&frasl;problemset&frasl;all"        
+# "&&gt;"
+# "&&&"
