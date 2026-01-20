2ec9a3f5e950560c7895316c6fda99a8450a6120
sw6820 <52646313+sw6820@users.noreply.github.com>
2026-01-20 20:43:14 +0900

    Time: 11 ms (90.86%), Space: 20.1 MB (6.52%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.py
@@ -1,11 +1,12 @@
 class Solution:    
     def maxDistinct(self, s: str) -> int:
-        from collections import defaultdict
-        d=defaultdict(bool)
-        for c in s:
-            if not d[c]:
-                d[c]=True
-        return len(d)
+        # from collections import defaultdict
+        # d=defaultdict(bool)
+        # for c in s:
+        #     if not d[c]:
+        #         d[c]=True
+        # return len(d)
+        return len(set(s))
 
         
 
